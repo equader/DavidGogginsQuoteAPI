f5e40341d6c1ce6eb115a244e3cf441ce09ede91
eshaan_mahimkar <eshaan.mahimkar@student.uts.edu.au>
2023-11-02 22:45:15 +1100

    Optimised fr

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Read and parse the JSON data file
+const quotesData = JSON.parse(fs.readFileSync('davidgogginsquote.json', 'utf8'));
 
-const fs = require('fs');
+// Utility function to get a random element from an array
+const getRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-// Read the JSON file with your data
-const rawData = fs.readFileSync('davidgogginsquote.json');
-const quotesData = JSON.parse(rawData);
-
-
-// Get a random quote
+// Endpoint to get a random quote from any category
 app.get('/api/quotes', (req, res) => {
-  const randomCategory = quotesData.categories[Math.floor(Math.random() * quotesData.categories.length)];
-  const randomQuote = randomCategory.quotes[Math.floor(Math.random() * randomCategory.quotes.length)];
+  const randomCategory = getRandomElement(quotesData.categories);
+  const randomQuote = getRandomElement(randomCategory.quotes);
   res.json(randomQuote);
 });
 
-// Get a random quote from a specific category
+// Endpoint to get a random quote from a specific category
 app.get('/api/quotes/category/:category/random', (req, res) => {
-    const category = req.params.category;
-    const categoryData = quotesData.categories.find((cat) => cat.category === category);
+  const categoryData = quotesData.categories.find((cat) => cat.category === req.params.category);
   
-    if (!categoryData) {
-      return res.status(404).json({ error: 'Category not found' });
-    }
-  
-    const randomQuote = categoryData.quotes[Math.floor(Math.random() * categoryData.quotes.length)];
-    res.json(randomQuote);
-  });
-  
+  if (!categoryData) {
+    return res.status(404).json({ error: 'Category not found' });
+  }
 
-// Get a list of available categories
+  const randomQuote = getRandomElement(categoryData.quotes);
+  res.json(randomQuote);
+});
+
+// Endpoint to get a list of available categories
 app.get('/api/categories', (req, res) => {
   const categories = quotesData.categories.map((cat) => cat.category);
   res.json(categories);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
